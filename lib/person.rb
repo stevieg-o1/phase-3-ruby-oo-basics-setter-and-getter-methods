5cce17a96bb5808be5bbabfcc584d26844f52d1a
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-10 12:36:21 +0300

    completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+    def name
+        @name
+    end
+    def name=(person_name)
+        @name=person_name
+    end
+
+    def job
+        @job
+    end
+    def job=(person_job)
+        @job = person_job
+    end
 
 end
